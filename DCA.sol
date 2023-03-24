6e26bcb11d1824cd5ca9a70a65ebd1c2e33e239f
nmorrissette244 <nmorrissette244@gmail.com>
2023-03-24 10:31:44 -0500

    Initialize project

---
+++ DCA.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+contract DCA {
+    constructor(
+        address _token,
+        uint256 _amount,
+        uint256 _interval,
+        uint256 _duration,
+        address _owner
+    ) {}
+
+    function deposit() public payable {}
+}
---
+++ DCAFactory.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+import "./DCA.sol";
+
+contract DCAFactory {
+    // Array of all deployed DCA contracts
+    DCA[] public deployedDCA;
+
+    // Map each deployed DCA contract to its owner
+    mapping(address => address) public dcaToOwner;
+
+    function createDCA(
+        address _token,
+        uint256 _amount,
+        uint256 _interval,
+        uint256 _duration
+    ) public {
+        DCA newDCA = new DCA(_token, _amount, _interval, _duration, msg.sender);
+        deployedDCA.push(newDCA);
+    }
+}
